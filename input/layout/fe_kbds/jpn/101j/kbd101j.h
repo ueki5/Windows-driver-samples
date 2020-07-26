bd0920a19a847b65ea4d903f9d07b6467cb6dd99
ueki5 <ueki005@gmail.com>
2020-07-26 17:33:46 +0900

    cosmetic directory and file

--- input/layout/fe_kbds/jpn/101j/kbd101j.h
+++ input/layout/fe_kbds/jpn/101j/kbd101j.h
@@ -18,3 +18,35 @@
  */
 #include "kbd.h"
 #include <dontuse.h>
+ /***************************************************************************\
+ * The table below defines the virtual keys for various keyboard types where
+ * the keyboard differ from the US keyboard.
+ *
+ * _EQ() : all keyboard types have the same virtual key for this scancode
+ * _NE() : different virtual keys for this scancode, depending on kbd type
+ *
+ *     +------+ +----------+----------+----------+----------+----------+----------+
+ *     | Scan | |    kbd   |    kbd   |    kbd   |    kbd   |    kbd   |    kbd   |
+ *     | code | |   type 1 |   type 2 |   type 3 |   type 4 |   type 5 |   type 6 |
+ \****+-------+_+----------+----------+----------+----------+----------+----------+*/
+
+#undef  T0D
+#define T0D _NE(OEM_7,     OEM_PLUS,  OEM_PLUS,  OEM_PLUS,  OEM_PLUS,  OEM_PLUS,  OEM_PLUS  )
+#undef  T1A
+#define T1A _NE(OEM_4,     OEM_4,     OEM_4,     OEM_4,     OEM_4,     OEM_4,     OEM_4     )
+#undef  T1B
+#define T1B _NE(OEM_6,     OEM_6,     OEM_6,     OEM_6,     OEM_6,     OEM_6,     OEM_6     )
+#undef  T27
+#define T27 _NE(OEM_PLUS,  OEM_1,     OEM_1,     OEM_1,     OEM_1,     OEM_1,     OEM_1     )
+#undef  T28
+#define T28 _NE(OEM_1,     OEM_7,     OEM_7,     OEM_7,     OEM_7,     OEM_7,     OEM_7     )
+#undef  T29
+#define T29 _NE(OEM_3,     OEM_3,     OEM_3,     OEM_3,     OEM_3,     OEM_3,     OEM_3     )
+#undef  T2B
+#define T2B _NE(OEM_5,     OEM_5,     OEM_5,     OEM_5,     OEM_5,     OEM_5,     OEM_5     )
+#undef  T3A
+#define T3A _NE(DBE_ALPHANUMERIC,CAPITAL,CAPITAL,CAPITAL,CAPITAL,CAPITAL,CAPITAL   )
+#undef  T56
+#define T56 _NE(_none_,    OEM_102,   OEM_102,   OEM_102,   OEM_102,   OEM_102,   OEM_102   )
+#undef  T5C
+#define T5C _NE(NONAME,    DBE_SBCSCHAR, OEM_AX,    OEM_JUMP,  OEM_JUMP,  OEM_JUMP,  OEM_JUMP  )
